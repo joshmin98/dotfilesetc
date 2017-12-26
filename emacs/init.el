11628543c4298b1f088bd54d41e9bb29835b937b
Josh <minje@usc.edu>
2017-12-25 20:51:35 -0800

    Refactored and updated emacs config.

--- emacs/init.el
+++ emacs/init.el
@@ -1,31 +1,12 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+	          '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
-
-;;packages
-(org-babel-load-file (expand-file-name "./joshminit.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (flymake-cursor-mode zenburn-theme yasnippet which-key vue-mode use-package tablist skewer-mode rjsx-mode ranger racer projectile powerline ox-twbs org-bullets nlinum neotree magit intero iedit google-c-style glsl-mode flymake-rust flycheck-rust evil-tutor emmet-mode counsel company-tern company-irony company-c-headers android-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file (expand-file-name "~/.emacs.d/joshminit.org"))
